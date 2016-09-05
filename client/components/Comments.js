d7241328e87b06f954a05d6f5caec3f3202d7f98
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-09-05 17:13:18 -0600

    add comments component

---
+++ client/components/Comments.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Comments = React.createClass({
+  renderComment(comment, index){
+    return (
+      <div className="comment" key={index}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className="remove-comment">&times;</button>
+        </p>
+      </div>
+    )
+  },
+  render(){
+    return (
+      <div className="comments">
+        {this.props.postComments.map(this.renderComment)}
+        <form ref="commentForm" className="comment-form">
+          <input type="text" ref="author" placeholder="author"/>
+          <input type="text" ref="comment" placeholder="comment"/>
+          <input type="submit" hidden/>
+        </form>
+      </div>
+    );
+  }
+});
+
+export default Comments;
--- client/components/Single.js
+++ client/components/Single.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
   render(){
+    const postId = this.props.params.postId
+    const index = this.props.posts.findIndex((post) => post.code === postId);
+    const post = this.props.posts[index];
+    const postComments = this.props.comments[postId] || [];
     return(
       <div className="single-photo">
-         "I'm the single"
+        <Photo index={index} post={post} {...this.props}/>
+        <Comments postComments={postComments}/>
       </div>
     );
   }
